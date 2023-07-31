6651c410877bf5290f904bb6a07dbe7e1e263ce9
MarkPeatfield <67265933+MarkPeatfield@users.noreply.github.com>
2023-07-31 08:06:47 +0800

    Changed for Asahi

--- push_to_github.sh
+++ push_to_github.sh
@@ -3,7 +3,15 @@
 #this is intended to publish the glossary to the blog
 
 # Define the repository path
-repo_path="/Users/markpeatfield/ego-machine-pages/"
+#repo_path="/Users/markpeatfield/ego-machine-pages/"
+
+# Define the repository path based on USER variable
+if [ $USER == "markpeatfield" ]; then
+  repo_path="/Users/markpeatfield/ego-machine-pages/"
+else
+  repo_path="/home/mark/ego-machine-pages/"
+fi
+
 
 # Change directory to the repository
 cd $repo_path
@@ -18,3 +26,5 @@ git commit -m "Commit changes"
 git push https://github.com/MarkPeatfield/ego-machine-pages.git
 # Display successful message
 echo "Push to remote repository completed"
+
+
